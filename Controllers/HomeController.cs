a01469677823a8c788132688269d56d92ab3d02c
Eric <Eric@ERIC-LAPTOP>
2021-04-20 01:03:38 +0800

    加强提示功能

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace Stusign.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(String successMsg)
         {
             uid = Convert.ToInt16(User.Identity.Name);
             var stuinfo = await _context.Stuinfo.AsNoTracking().FirstAsync(e => e.编号==uid);
@@ -38,16 +38,23 @@ namespace Stusign.Controllers
 
             IndexViewModel _indexView = new IndexViewModel();
             _indexView.From(stuinfo,_systemOptions);
+            _indexView.提示信息 = successMsg;
             return View(_indexView);
         }
 
         [HttpPost]
         public async Task<IActionResult> SaveInfo(IndexViewModel indexView)
         {
+            uid = Convert.ToInt16(User.Identity.Name);
+            var stuinfo = await _context.Stuinfo.FirstAsync(e => e.编号==uid);
+
             if (!ModelState.IsValid)
             {
-                var c = ModelState.Keys;
-                return RedirectToAction("Index","Account",new{error = "您的数据不符合规范，被判定为有攻击行为，已将您注销登录！"});
+                ViewBag.FileSizeMax = 5;
+                ViewBag.Avatar = (stuinfo.头像文件 == "")||(stuinfo.头像文件 == null)?"":Url.Action("Avatar","File");
+                ViewBag.Print = _systemOptions.打印开放 && (bool)stuinfo.审核结果;
+                indexView.错误信息 = "您的数据不符合规范，请检查后再提交！";
+                return View("Index", indexView);
             }
 
             var sum = Convert.ToInt16(indexView.五县三好)  + Convert.ToInt16(indexView.五县优干) +Convert.ToInt16(indexView.五县优少) +
@@ -56,9 +63,6 @@ namespace Stusign.Controllers
                       Convert.ToInt16(indexView.六县三好)  + Convert.ToInt16(indexView.六县优干) +Convert.ToInt16(indexView.六县优少) +
                       Convert.ToInt16(indexView.六市三好) + Convert.ToInt16(indexView.六市优干) + Convert.ToInt16(indexView.六市优少) +
                       Convert.ToInt16(indexView.六校三好) + Convert.ToInt16(indexView.六校优干) + Convert.ToInt16(indexView.六校优少);
-
-            uid = Convert.ToInt16(User.Identity.Name);
-            var stuinfo = await _context.Stuinfo.FirstAsync(e => e.编号==uid);
 
             if (sum > 24)
             {
@@ -79,7 +83,7 @@ namespace Stusign.Controllers
                 indexView.To(stuinfo);
                 await _context.SaveChangesAsync();
                 //return Content(JsonConvert.SerializeObject(indexView));
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index),new {successMsg = "您的信息已经提交成功，您可以在信息锁定前多次修改信息"});
             }
         }
 
